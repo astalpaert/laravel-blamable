0215d84db338aedd8a322b4ba58ca99f9dca1620
Ken Andries <ken.andries.1992@gmail.com>
2022-10-06 21:31:43 +0200

    Fix remove blamable fields on SQLite (Resovles #6)

--- src/Database/RemoveBlamableFieldsMacro.php
+++ src/Database/RemoveBlamableFieldsMacro.php
@@ -3,7 +3,6 @@
 namespace Astalpaert\LaravelBlamable\Database;
 
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 /**
  * @mixin Blueprint
@@ -13,19 +12,7 @@ class RemoveBlamableFieldsMacro
     public function __invoke()
     {
         return function () {
-            $tableName = $this->getTable();
-
-            if (Schema::hasColumn($tableName, 'created_by')) {
-                $this->dropColumn('created_by');
-            }
-
-            if (Schema::hasColumn($tableName, 'updated_by')) {
-                $this->dropColumn('updated_by');
-            }
-
-            if (Schema::hasColumn($tableName, 'deleted_by')) {
-                $this->dropColumn('deleted_by');
-            }
+            $this->dropColumn(['created_by', 'updated_by', 'deleted_by']);
         };
     }
 }
